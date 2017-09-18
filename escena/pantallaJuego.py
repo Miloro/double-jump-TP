bbf3dc154c81114ed05fd48ecf0cdc03ca625e5e
Miloro <miloromiguel@gmail.com>
2017-09-17 23:32:25 -0300

    se cambio el nombre de player por jugador, se modifico la escena juego y el main

--- escena/pantallaJuego.py
+++ escena/pantallaJuego.py
@@ -1,12 +1,27 @@
 import pilasengine
-from actor.player import Player
+from actor.jugador import Jugador
+from actor.plataforma import Plataforma
+from actor.plataformaConMovimiento import PlataformaCM
 
 class PantallaJuego(pilasengine.escenas.Escena):
 
 
     def iniciar(self, pilas):
         self.fondo = pilas.fondos.FondoMozaico("imagenes/fondo/madera.jpg")
-        self.jugador = Player(pilas)
+        self.jugador = Jugador(pilas)
+        self.plataforma = Plataforma(pilas)
+        self.plataformaCMn = PlataformaCM(pilas)
+        self.jugador.y = 400
+        self.plataforma.y = -180
+        self.plataformaCMn.y = 300 
+        self.plataforma.y = [-100, 100, -100, 100,-100, 100,-100, 100,-100, 100,-100]
 
+    def crearPlataformas():
+    	plataformaCM = PlataformaCM()
+    	plataformaCM = 400
 
+   # def actualizar():
+    #	self.tarea = self.pilas.tareas.siempre(0.1, self.crearPlataformas)
 
+   # def actualizar():
+        #self.tarea = self.pilas.tareas.siempre(0.1, crearPlataformas)
--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from escena.pantallaJuego import PantallaJuego
 
 pilas = pilasengine.iniciar(titulo='double-jump 0.1 - alpha', habilitar_mensajes_log=False)
 
-
-
-
 pilas.escenas.vincular(PantallaJuego)
 pilas.escenas.PantallaJuego(pilas=pilas)
 pilas.ejecutar()
